329921b36c333e5e788545b20173f4c4ed9a09e5
Yaprak Sıla Öztürk <106006892+slazturk@users.noreply.github.com>
2024-04-01 23:45:17 +0300

    online shopping

---
+++ onlineShopping-gateway/src/main/java/com/patika/onlineShoppinggateway/OnlineShoppingGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.patika.onlineShoppinggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShoppingGatewayApplication.class, args);
+    }
+
+}
---
+++ onlineShopping-gateway/src/test/java/com/patika/onlineShoppinggateway/KredinbizdeGwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patika.onlineShoppinggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KredinbizdeGwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
